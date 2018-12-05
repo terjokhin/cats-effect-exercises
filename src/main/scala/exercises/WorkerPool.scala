396ee2c5fbb3c9fccf061f2673aa4afc8ddcbc89
Aleksei <daron666@yandex.ru>
2018-12-05 11:05:45 +0400

    Fix worker pool issue, and some syntax cleanup.

--- src/main/scala/exercises/WorkerPool.scala
+++ src/main/scala/exercises/WorkerPool.scala
@@ -1,10 +1,13 @@
 package exercises
 
+import cats.data.NonEmptyList
 import cats.effect._
 import cats.effect.concurrent.{MVar, Ref}
+import cats.effect.implicits._
 import cats.syntax.applicative._
 import cats.syntax.flatMap._
 import cats.syntax.functor._
+import cats.syntax.traverse._
 
 import scala.concurrent.duration._
 import scala.util.Random
@@ -12,7 +15,9 @@ import scala.util.Random
 
 object WorkerPool extends IOApp {
 
-  type Worker[F[_], A, B] = A => F[B]
+  private def printOut[F[_]](s: String)(implicit C: Concurrent[F]): F[Unit] = C.delay(println(s))
+
+  final case class Worker[F[_], A, B](run: A => F[B], id: Int)
 
   def mkWorker[F[_]](id: Int)(implicit T: Timer[F], C: Concurrent[F]): F[Worker[F, Int, Int]] =
     Ref[F].of(0).map { counter =>
@@ -21,10 +26,12 @@ object WorkerPool extends IOApp {
 
       def report: F[Unit] = for {
         i <- counter.get
-        _ <- C.delay(println(s"Total processed by $id: $i"))
+        _ <- printOut(s"Total processed by $id: $i")
       } yield ()
 
-      x => simulateWork >> counter.update(_ + 1) >> report >> C.pure(x + 1)
+      val work: Int => F[Int] = x => simulateWork >> counter.update(_ + 1) >> report >> C.pure(x + 1)
+
+      Worker(work, id)
     }
 
   trait WorkerPool[F[_], A, B] {
@@ -37,60 +44,59 @@ object WorkerPool extends IOApp {
 
   object WorkerPool {
 
-    def of[F[_] : Concurrent : Timer, A, B](fs: List[Worker[F, A, B]]): F[WorkerPool[F, A, B]] = for {
-      workers <- MVar.of[F, List[Worker[F, A, B]]](fs)
-      workersPool = PoolImpl(workers)
+    def of[F[_] : Concurrent : Timer, A, B](fs: NonEmptyList[Worker[F, A, B]]): F[WorkerPool[F, A, B]] = for {
+      workersQueue <- MVar.empty[F, Worker[F, A, B]]
+      _ <- fs.map(workersQueue.put).map(_.start.void).sequence
+      active <- Ref[F].of(fs.map(_.id).toList)
+      workersPool = PoolImpl(workersQueue, active)
     } yield workersPool
 
 
-    private def addFreeWorker[F[_] : Concurrent, A, B](workers: MVar[F, List[Worker[F, A, B]]], worker: Worker[F, A, B]): F[Unit] =
-      workers.take.flatMap { list =>
-        Concurrent[F].start(workers.put(list :+ worker)) >> Concurrent[F].unit
-      }
+    private def getBack[F[_] : Concurrent, A, B](workersQueue: MVar[F, Worker[F, A, B]],
+                                                 worker: Worker[F, A, B],
+                                                 activeIdsRef: Ref[F, List[Int]]): F[Unit] = for {
+      isActive <- activeIdsRef.get.map(_.contains(worker.id))
+      result <- if (isActive) workersQueue.put(worker).start.void else Concurrent[F].unit
+    } yield result
 
-    private case class PoolImpl[F[_] : Concurrent : Timer, A, B](workers: MVar[F, List[Worker[F, A, B]]]) extends WorkerPool[F, A, B] {
-      override def exec(a: A): F[B] = workers.take.flatMap {
-        case x :: xs =>
-          workers.put(xs) >> Concurrent[F].guarantee(x(a))(addFreeWorker(workers, x))
-        case Nil =>
-          Concurrent[F].delay(println("No workers. Let's wait for available.")) >>
-            workers.put(List.empty) >>
-            Timer[F].sleep(500 milliseconds) >>
-            Concurrent[F].defer(exec(a))
-      }
+
+    private case class PoolImpl[F[_] : Concurrent, A, B](workersQueue: MVar[F, Worker[F, A, B]],
+                                                         activeIds: Ref[F, List[Int]]) extends WorkerPool[F, A, B] {
+      override def exec(a: A): F[B] = for {
+        w <- workersQueue.take
+        result <- w.run(a).guarantee(getBack(workersQueue, w, activeIds))
+      } yield result
 
       override def removeAllWorkers: F[Unit] =
-        workers.take >> workers.put(List.empty) >> Concurrent[F].delay(println("All workers have been removed."))
+        activeIds.set(List.empty[Int]) >> printOut("All workers have been removed.")
 
       def addWorker(worker: Worker[F, A, B]): F[Unit] =
-        addFreeWorker(workers, worker) >> Concurrent[F].delay(println("New worker added"))
+        activeIds.update(_ :+ worker.id) >> workersQueue.put(worker) >> printOut("New worker added")
     }
-
   }
 
   def getNewWorker(ids: Ref[IO, Int]): IO[Worker[IO, Int, Int]] = for {
-    worker <- ids.get.flatMap(mkWorker[IO](_)).guarantee(ids.update(_ + 1))
+    worker <- ids.get.flatMap(mkWorker[IO](_))
+    _ <- ids.update(_ + 1)
   } yield worker
 
   def getNWorkers(n: Int, ids: Ref[IO, Int]): IO[List[Worker[IO, Int, Int]]] = getNewWorker(ids).replicateA(n)
 
-  // Sample test pool to play with in IOApp
   def testPool(ids: Ref[IO, Int]): IO[WorkerPool[IO, Int, Int]] = for {
-    workers <- getNWorkers(10, ids)
-    pool <- WorkerPool.of[IO, Int, Int](workers)
+    workers <- getNWorkers(5, ids)
+    pool <- WorkerPool.of[IO, Int, Int](NonEmptyList.fromListUnsafe(workers))
   } yield pool
 
   def run(args: List[String]) =
     for {
       ids <- Ref[IO].of(0)
       pool <- testPool(ids)
-      _ <- pool.exec(42).replicateA(20)
-      _ <- pool.removeAllWorkers
       work <- pool.exec(42).replicateA(20).start
-      newWorker1 <- getNewWorker(ids)
-      _ <- pool.addWorker(newWorker1)
-      newWorker2 <- getNewWorker(ids)
-      _ <- pool.addWorker(newWorker2)
+      _    <- pool.removeAllWorkers
+      w <- getNewWorker(ids)
+      _ <- pool.addWorker(w)
+      v <- getNewWorker(ids)
+      _ <- pool.addWorker(v)
       _ <- work.join
     } yield ExitCode.Success
 
